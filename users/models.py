4186f32175698b9c1e74305429324f7315524476
Atiar-Rahman <atiar1709@gmail.com>
2026-04-06 12:09:21 +0600

    custom user setup

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUserManager
 
-# Create your models here.
+# CustomUserModel create inheriate abstract user
+
+class User(AbstractUser):
+    username = 'None'
+    email = models.EmailField(unique=True)
+    first_name=models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100,null=True, blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # username remove must be override objects manager
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
